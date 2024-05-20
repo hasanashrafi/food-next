5e4733dcd25290e148565ff1ddafa39bdeab4b2d
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-20 19:57:35 +0330

    create home page and banner in homePage

---
+++ components/templates/HomePage.js
@@ -0,0 +1,11 @@
+import Banner from "../modules/Banner"
+
+function HomePage() {
+    return (
+        <div className=" flex flex-wrap justify-between p-4 ">
+            <Banner/>
+        </div>
+    )
+}
+
+export default HomePage
