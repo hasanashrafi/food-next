c5604a9154edcfacc4d4038d2abaa5ff46af2261
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-21 10:58:17 +0330

    added attributes component and definition to home page

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,9 +1,13 @@
+import Attributes from "../modules/Attributes"
 import Banner from "../modules/Banner"
+import Definition from "../modules/Definition"
 
 function HomePage() {
     return (
         <div className=" flex flex-wrap justify-between p-4 ">
-            <Banner/>
+            <Banner />
+            <Attributes />
+            <Definition />
         </div>
     )
 }
