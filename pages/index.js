f644951bff6d4d10ad019ac588071856ad005456
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-21 10:58:41 +0330

    edit styles

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import HomePage from "@/components/templates/HomePage";
 export default function Home() {
   return (
     <main
-      className={` max-w-6xl m-auto min-h-screen bg-red-500  p-4 `}>
-      <HomePage/>
+      className={` max-w-6xl m-auto min-h-screen  p-4 `}>
+      <HomePage />
     </main>
   );
 }
