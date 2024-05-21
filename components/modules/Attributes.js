ce1f4b7df3f0daf499a41347b84244e00aa8c63a
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-21 10:57:08 +0330

    create attribute component

--- components/modules/Attributes.js
+++ components/modules/Attributes.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import Choice from "../icons/Choice";
+import Clock from "../icons/Clock";
+import Fast from "../icons/Fast";
+import Food from "../icons/Food";
+
 
 function Attributes() {
     return (
-        <div>
-            Attributes
+        <div className='w-full mt-24 '>
+            <h3 className="text-lg text-[#53c60b] my-6">Why us?</h3>
+            <div className='flex justify-between text-center place-items-center'>
+                <div className="flex flex-col  items-center w-40 rounded-xl border-b-2 shadow-md shadow-[#53c60b] border-b-[#53c60b] text-center py-3 px-3 mt-18 ">
+                    <Fast />
+                    <p className="font-semibold mt-5">Fast</p>
+                </div>
+                <div className="flex flex-col  items-center w-40 rounded-xl border-b-2 shadow-md shadow-[#53c60b] border-b-[#53c60b] text-center py-3 px-3 mt-18 ">
+                    <Food />
+                    <p className="font-semibold mt-5">Best Restaurants</p>
+                </div>
+                <div className="flex flex-col  items-center w-40 rounded-xl border-b-2 shadow-md shadow-[#53c60b] border-b-[#53c60b] text-center py-3 px-3 mt-18 ">
+                    <Choice />
+                    <p className="font-semibold mt-5">Your Choice</p>
+                </div>
+                <div className="flex flex-col  items-center w-40 rounded-xl border-b-2 shadow-md shadow-[#53c60b] border-b-[#53c60b] text-center py-3 px-3 mt-18 ">
+                    <Clock />
+                    <p className="font-semibold mt-5">24-7</p>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Attributes
+export default Attributes;
