92161c8b6f3bec55ba9e0bc5240083ffb9a93e59
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-21 11:21:21 +0330

    adit styles

--- components/modules/Attributes.js
+++ components/modules/Attributes.js
@@ -6,7 +6,7 @@ import Food from "../icons/Food";
 
 function Attributes() {
     return (
-        <div className='w-full mt-24 '>
+        <div className='w-full mt-24 m-auto '>
             <h3 className="text-lg text-[#53c60b] my-6">Why us?</h3>
             <div className='flex justify-between text-center place-items-center'>
                 <div className="flex flex-col  items-center w-48 h-20 sm:h-36 m-2  rounded-xl border-b-2 shadow-md shadow-[#53c60b] border-b-[#53c60b] text-center py-3 px-3 mt-18 ">
