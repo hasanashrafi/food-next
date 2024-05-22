0c80fb2cfdcd6ce21596742d35b072db43a7f6f6
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-22 12:41:26 +0330

    create restriction component

---
+++ components/modules/Restriction.js
@@ -0,0 +1,17 @@
+
+
+function Restrictions() {
+  return (
+    <div className='mt-24 mx-0 mb-52'>
+      <h3 className="text-lg text-[#53c60b] my-6 mx-0">Restrictions</h3>
+      <p className="text-md font-normal text-justify leading-7">
+        Prohibited items. Merchants may only offer to sell items expressly
+        permitted by their agreement with Uber. A merchant cannot offer
+        specially regulated or illicit items, like cannabinol (CBD) and
+        tetrahydrocannabinol (THC), on their Uber Eats menu.
+      </p>
+    </div>
+  );
+}
+
+export default Restrictions;
