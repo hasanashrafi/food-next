2e4a7b54862c8ab43b447e1c020ba4f9358a1b2a
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-26 09:21:08 +0330

    edit styles

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,14 +5,13 @@ function Layout({ children }) {
     return (
         <>
             <header className='flex flex-wrap  w-full  h-fit place-items-center bg-[#004225] justify-between'>
-                <div className='p-2 text-2xl sm:text-4xl w-1/4 sm:w-fit font-semibold text-[#F5F5DC]  m-2'>
+                <div className='p-2 text-2xl  w-1/4 sm:w-fit font-semibold text-[#F5F5DC]  m-2'>
                     <Link href='/' className='hover:text-white'>YEMEK</Link>
                 </div>
                 <div className='flex flex-col  text-[#FFB000] py-2 text-center justify-center'>
-                    <GiFullPizza className='text-5xl sm:text-7xl self-center' />
+                    <GiFullPizza className='text-5xl  self-center' />
                 </div>
-
-                <div className='flex flex-wrap justify-around p-2 text-[#F5F5DC]  w-1/10 sm:w-48 md:w-48 xl:w-48 text-sm sm:text-xl sm:font-semibold   m-2'>
+                <div className='flex flex-wrap justify-around p-2 text-[#F5F5DC]  w-1/10 sm:w-48 md:w-48 xl:w-48 text-sm  sm:font-semibold   m-2'>
                     <Link href='/menu' className='hover:text-white ' >Menu</Link>
                     <Link href='/categories' className='hover:text-white ml-2 '>Categories</Link>
                 </div>
