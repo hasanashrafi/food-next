859985400904a144b737a1cb08f4b601cae71e55
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-30 13:23:25 +0330

    edit styles

--- components/templates/DetailsPage.js
+++ components/templates/DetailsPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaLocationDot } from 'react-icons/fa6'
+import { FaBasketShopping, FaLocationDot } from 'react-icons/fa6'
 import { MdAttachMoney } from 'react-icons/md'
 import { RiDiscountPercentLine } from 'react-icons/ri'
 import { IoFastFoodOutline } from "react-icons/io5";
@@ -17,11 +17,13 @@ function DetailsPage(data) {
 
     return (
         <div className=' m-auto mt-50 w-full h-full p-2'>
-            <h2 className='font-semibold text-xl border-b-2 mb-10 w-fit border-b-green-700'>Details</h2>
+            <h2 className='font-semibold text-xl border-b-2 mb-10 w-fit border-b-green-700'>
+            Details
+            </h2>
            
             <div className='flex flex-row'>
                 <div className='flex my-5 p-3'>
-                    <img src={`/images/${id}.jpeg`} alt={name} className='w-96 rounded ' />
+                    <img src={`/images/${id}.jpeg`} alt={name} className='w-96 rounded hover:scale-95 ease-in duration-150' />
 
                     <div className='w-full p-1 items-start ml-7 place-items-center '>
                         <h3 className='flex  font-semibold text-[#53c60b]'>
@@ -51,7 +53,10 @@ function DetailsPage(data) {
                                 )
                                 : null
                         }
+                <button className='flex w-38 hover:bg-white  hover:border-blue-700 hover:border hover:text-blue-800 ease-in duration-75 items-center place-content-center p-2 bg-blue-600 rounded my-4 text-white'>Add To Cart <FaBasketShopping className='ml-2 mb-1 text-lg'/>
+                </button>
                     </div>
+                    
                 </div>
             </div>
             <div>
@@ -60,27 +65,27 @@ function DetailsPage(data) {
                 </div>
                 <div className='mb-14'>
                     <h4 className='font-bold text-xl my-5 text-[#53c60b]'>Details</h4>
-                    <ul className=''>
+                    <ul className='flex justify-around bg-[#53c60b] rounded w-full  p-4 m-auto'>
                         {details.map((detail, index) =>
-                            <li key={index} className='flex items-center my-2 '>
-                                <p className='text-lg mx-1'>{Object.keys(detail)[0]}: </p>
+                            <li key={index} className='flex items-center my-2  '>
+                                <p className='text-md text-gray-800 mx-1 font-semibold'>{Object.keys(detail)[0]}: </p>
                                 <span className='text-gray-600'> {Object.values(detail)[0]}</span>
                             </li>
                         )}
                     </ul>
                 </div>
-                <div className='flex flex-col m-auto  w-fit text-center place-content-center p-4 align-baseline bg-[#53c60b] rounded'>
-                    <h4 className='font-semibold text-xl mb-1 p-2 text-center rounded text-white bg-[#307008]'>Ingredients</h4>
+                <div className='flex flex-col m-auto  w-full text-center place-content-center  align-baseline bg-[#53c60b] rounded'>
+                    <h4 className=' font-semibold text-xl mb-1 p-3 text-center  text-white bg-[#307008]'>Ingredients</h4>
                     <ul className=' m-3 p-1 text-gray-700 text-md'>
                         {ingredients.map((ingredient, index) =>
-                            <li key={index} className={`${index % 2 ? "bg-[#53c60b]" : "bg-[#408f10] text-white"} p-2 rounded`}>
+                            <li key={index} className={`${index % 2 ? "bg-[#53c60b]" : "bg-[#408f10] text-white"} p-2 rounded text-lg `}>
                                 <p >{ingredient}</p>
                             </li>
                         )}
                     </ul>
                 </div>
                 <div className=''>
-                    <h4>Recipe:</h4>
+                    <h4 className='text-center p-3 bg-[#307008] text-white mt-10 text-xl'>Recipe</h4>
                     {recipe.map((item, index) =>
                         <div key={index} className={`w-full place-items-center p-2 ${index % 2 ? "bg-[#53c60b]" : "bg-[#87f343]"} flex text-lg `}>
                             <span className='text-2xl mr-5 font-bold  p-2 '>
@@ -90,7 +95,7 @@ function DetailsPage(data) {
                         </div>
                     )}
                 </div>
-                <button className='p-3 bg-blue-600 rounded my-4 text-white'>Add To Cart</button>
+                
             </div>
 
         </div>
