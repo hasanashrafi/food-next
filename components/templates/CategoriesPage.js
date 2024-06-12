5f5a1cc3177450449473f63ba84340af9ed65f97
Hasan Ashrafi <yasashrafi@gmail.com>
2024-06-12 20:10:32 +0330

    create categoriesPage component and add options

---
+++ components/templates/CategoriesPage.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+function CategoriesPage() {
+    const [query, setQuery] = useState({ difficulty: "", time: "" });
+
+
+
+    const changeHandler = (e) => {
+        setQuery({ ...query, [e.target.name]: e.target.value });
+    }
+
+    const searchHandler = () => {
+        console.log(query);
+    }
+
+    return (
+        <div className='max-w-[900px] m-auto mt-[100px] min-h-screen'>
+            <h2 className='border-b-4 border-b-[#53c60b] w-fit mb-[50px] text-lg'> Categories</h2>
+            <div className=''>
+                <div className=''>
+                    <select
+                        value={query.difficulty}
+                        name="difficulty"
+                        onChange={changeHandler}
+                        className=' border-none w-[150px] h-[40px] rounded-md mr-[10px] p-[10px] text-[#48ac0a] shadow-lg outline-none'>
+                        <option value="" selected>
+                            Difficulty
+                        </option>
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+
+                    <select
+                        value={query.time}
+                        name="time"
+                        onChange={changeHandler}
+                        className='border-none w-[150px] h-[40px] rounded-md mr-[10px] p-[10px] text-[#48ac0a] shadow-lg outline-none'>
+                        <option value="" selected>Cooking Time</option>
+                        <option value="more">More Than 30 min </option>
+                        <option value="less">Less Than 30 min </option>
+                    </select>
+                    <button
+                        onClick={searchHandler}
+                        className='border-none bg-[#53c60b] text-white h-[39px] px-5 rounded-md cursor-pointer shadow-lg hover:bg-white hover:transition-all hover:text-[#53c60b]'>Search</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CategoriesPage
