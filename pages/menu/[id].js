fa0f56e5b3d76afdb89698fcdca07c6f975d56d4
Hasan Ashrafi <yasashrafi@gmail.com>
2024-06-13 13:12:58 +0330

    edit data api

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -14,12 +14,13 @@ export default Details
 
 export async function getStaticPaths() {
 
-    const res = await fetch('http://localhost:4000/data')
+    // const res = await fetch('http://localhost:4000/data')
+    // const json = await res.json()
+
+
+    const res = await fetch(`${process.env.BASE_URL}/data`)
     const json = await res.json()
 
-
-    // const res = await fetch(`${process.env.BASE_URL}/data`)
-    // const json = await res.json()
     const data = json.slice(0, 10)
     const paths = data.map(food => ({ params: { id: food.id.toString() } }))
 
@@ -33,11 +34,11 @@ export async function getStaticProps(context) {
     const { params: { id } } = context
 
 
-    const res = await fetch(`http://localhost:4000/data/${id}`)
-    const data = await res.json()
-
-    // const res = await fetch(`${process.env.BASE_URL}/data/${id}`)
+    // const res = await fetch(`http://localhost:4000/data/${id}`)
     // const data = await res.json()
+
+    const res = await fetch(`${process.env.BASE_URL}/data/${id}`)
+    const data = await res.json()
 
     if (!data.id) {
         return {
--- pages/menu/index.js
+++ pages/menu/index.js
@@ -10,12 +10,12 @@ export default Menu
 
 export async function getStaticProps() {
 
-  const res = await fetch('http://localhost:4000/data')
-  const data = await res.json()
-
-
-  // const res = await fetch(`${process.env.BASE_URL}/data`)
+  // const res = await fetch('http://localhost:4000/data')
   // const data = await res.json()
+
+
+  const res = await fetch(`${process.env.BASE_URL}/data`)
+  const data = await res.json()
 
   console.log(data);
 
