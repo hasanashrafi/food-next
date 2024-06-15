b494bcd4dea5b8bb2a43deceea5f4e2dcdb432cf
Hasan Ashrafi <yasashrafi@gmail.com>
2024-06-15 12:08:36 +0330

    edit styles

--- components/templates/DetailsPage.js
+++ components/templates/DetailsPage.js
@@ -8,23 +8,23 @@ function DetailsPage(data) {
         name,
         price,
         discount,
-        introduction, 
+        introduction,
         details,
         ingredients,
         recipe
     } = data
 
     return (
-        <div className=' m-auto mt-50 w-full h-full p-10'>
+        <div className=' m-auto mt-50 w-full h-full p-5'>
             <h2 className='font-semibold text-xl border-b-2 mb-10 w-fit border-b-green-700'>
                 Details
             </h2>
 
-            <div className='flex flex-row'>
-                <div className='flex my-5 p-3'>
-                    <img src={`/images/${id}.jpeg`} alt={name} className='w-[450px] rounded hover:scale-95 ease-in duration-150' />
+            <div className=''>
+                <div className='flex flex-col sm:flex-row my-5 p-3'>
+                    <img src={`/images/${id}.jpeg`} alt={name} className='w-full sm:w-[450px] rounded hover:scale-95 ease-in duration-150' />
 
-                    <div className='w-full p-1 items-start ml-7 place-items-center '>
+                    <div className='w-full p-1 items-start sm:ml-7 mt-5 place-items-center '>
                         <h3 className='flex  font-semibold text-[#53c60b]'>
                             {name}
                         </h3>
@@ -52,8 +52,8 @@ function DetailsPage(data) {
                                 )
                                 : null
                         }
-                        <button className='flex w-38 hover:bg-white hover:transition-all  hover:border-blue-700 hover:border hover:text-blue-800 ease-in duration-75 items-center place-content-center p-2 bg-blue-600 rounded my-4 text-white'>
-                        Add To Cart <FaBasketShopping className='ml-2 mb-1 text-lg' />
+                        <button className='flex w-full  sm:w-36 hover:bg-white hover:transition-all  hover:border-blue-700 hover:border hover:text-blue-800 ease-in duration-75 items-center place-content-center p-2 bg-blue-600 rounded my-4 text-white'>
+                            Add To Cart <FaBasketShopping className='ml-2 mb-1 text-lg' />
                         </button>
                     </div>
 
@@ -61,24 +61,24 @@ function DetailsPage(data) {
             </div>
             <div>
                 <div className='p-3  my-16 leading-6 text-justify'>
-                   <h1 className='font-bold'>introduction:</h1>
+                    <h1 className='font-bold'>introduction:</h1>
                     <p>{introduction}</p>
                 </div>
-                <div className='mb-14 mx-auto items-center text-center'>
-                    <h4 className='font-bold text-xl my-5 text-[#28ac17]'>
-                    Details
+                <div className='flex flex-wrap justify-center sm:w-full  w-fit mb-14 mx-auto items-center text-center'>
+                    <h4 className='font-bold text-xl w-full p-3 bg-[#307008] text-[#ffffff]'>
+                        Details
                     </h4>
-                    <ul className='flex justify-around bg-[#17f046]  rounded w-full  p-4 m-auto'>
+                    <ul className=' flex flex-col sm:flex-row sm:justify-around justify-center bg-[#4feb07]  rounded w-full  p-4 m-auto'>
                         {details.map((detail, index) =>
-                            <li key={index} className='flex items-center my-2  '>
-                                <p className='text-md text-[#1f361d] mx-1 font-semibold'>{Object.keys(detail)[0]}: </p>
+                            <li key={index} className='flex w-fit self-center items-center my-2  '>
+                                <p className='text-md text-[#ffffff] mx-1 font-semibold'>{Object.keys(detail)[0]}: </p>
                                 <span className='text-[#006769] font-semibold'> {Object.values(detail)[0]}</span>
                             </li>
                         )}
                     </ul>
                 </div>
-                <div className='flex flex-col m-auto  w-[550px] text-center place-content-center  align-baseline  rounded'>
-                    <h4 className=' font-semibold text-xl mb-1 p-3 text-center  text-white bg-[#307008]'>Ingredients</h4>
+                <div className='flex flex-col m-auto sm:w-[750px]  w-[350px] text-center place-content-center  align-baseline  rounded'>
+                    <h4 className=' font-semibold text-xl  p-3 text-center  text-white bg-[#307008]'>Ingredients</h4>
                     <ul className='  text-gray-700 text-md'>
                         {ingredients.map((ingredient, index) =>
                             <li key={index} className={`${index % 2 ? "bg-[#7af00b]" : "bg-[#0bc634] text-white"} p-2  text-lg `}>
@@ -87,10 +87,10 @@ function DetailsPage(data) {
                         )}
                     </ul>
                 </div>
-                <div className='w-[550px] mx-auto '>
+                <div className='sm:w-[750px] w-[350px] mx-auto '>
                     <h4 className='text-center p-3 bg-[#307008] text-white mt-10 text-xl'>Recipe</h4>
                     {recipe.map((item, index) =>
-                        <div key={index} className={`w-full place-items-center p-2 ${index % 2 ? "bg-[#7af00b]" : "bg-[#0bc634] text-white"} flex text-lg `}>
+                        <div key={index} className={`w-full place-items-center p-2 ${index % 2 ? "bg-[#7af00b]" : "bg-[#0bc634] text-white"} flex flex-col sm:flex-row text-lg `}>
                             <span className='text-2xl mr-5 font-bold  p-2 '>
                                 {index + 1}
                             </span>
